4d0a8f256348dc9c71740bd67d122fc362181370
Its-Jinxed <itsjinxed@outlook.com>
2026-04-21 15:48:47 +0100

    Change 'Password' label to 'Rating' in List.js

--- js/pages/List.js
+++ js/pages/List.js
@@ -51,7 +51,7 @@ export default {
                             <p>{{ level.id }}</p>
                         </li>
                         <li>
-                            <div class="type-title-sm">Password</div>
+                            <div class="type-title-sm">Rating</div>
                             <p>{{ level.rating }}</p>
                         </li>
                     </ul>
