6c484ddfe7b01e4b846096bb9d2fb3ee71d783fb
Its-Jinxed <itsjinxed@outlook.com>
2026-04-23 13:43:16 +0100

    Update List.js

--- js/pages/List.js
+++ js/pages/List.js
@@ -6,125 +6,135 @@ import { fetchEditors, fetchList } from "../content.js";
 import Spinner from "../components/Spinner.js";
 import LevelAuthors from "../components/List/LevelAuthors.js";
 
+const roleIconMap = {
+    owner: "crown",
+    admin: "user-gear",
+    helper: "user-shield",
+    dev: "code",
+    trial: "user-lock",
+};
+
 export default {
     components: { Spinner, LevelAuthors },
-
     template: `
         <main v-if="loading">
-            <Spinner />
+            <Spinner></Spinner>
         </main>
-
-        <main v-else class="page page-list">
-
-            <!-- LEFT LIST -->
-            <div class="page-sidebar list-container">
-
-                <div
-                    v-for="([level, err], i) in list"
-                    :key="i"
-                    class="ui-row level-row"
-                    :class="{ active: selected === i, error: !level }"
-                    @click="selected = i"
-                >
-                    <div class="type-label-lg">
-                        {{ i + 1 <= 150 ? '#' + (i + 1) : 'Legacy' }}
-                    </div>
-
-                    <img
-                        v-if="level?.youtubeId"
-                        :src="'https://img.youtube.com/vi/' + level.youtubeId + '/mqdefault.jpg'"
-                    />
-
-                    <div class="type-label-lg">
-                        {{ level?.name || 'Error (' + err + '.json)' }}
-                    </div>
-                </div>
-
+        <main v-else class="page-list">
+            <div class="list-container">
+                <table class="list" v-if="list">
+                    <tr v-for="([level, err], i) in list" :key="i">
+                        <td class="rank">
+                            <p v-if="i + 1 <= 150" class="type-label-lg">#{{ i + 1 }}</p>
+                            <p v-else class="type-label-lg">Legacy</p>
+                        </td>
+                        <td class="level" :class="{ 'active': selected == i, 'error': !level }">
+                            <button @click="selected = i">
+                                <img
+                                    v-if="level && level.youtubeId"
+                                    class="thumb"
+                                    :src="'https://img.youtube.com/vi/' + level.youtubeId + '/mqdefault.jpg?v=' + level.youtubeId"
+                                />
+                                <span class="type-label-lg">
+                                    {{ level?.name || 'Error (' + err + '.json)' }}
+                                </span>
+                            </button>
+                        </td>
+                    </tr>
+                </table>
             </div>
 
-            <!-- RIGHT PANEL -->
-            <div class="page-content level-detail">
-
-                <div v-if="level" class="ui-card">
-
+            <div class="level-container">
+                <div class="level" v-if="level">
                     <h1>{{ level.name }}</h1>
 
                     <LevelAuthors
                         :creators="level.creators"
                         :verifier="level.verifier"
-                    />
+                    ></LevelAuthors>
 
-                    <iframe
-                        class="level-video"
-                        :src="video"
-                        frameborder="0"
-                    ></iframe>
+                    <iframe class="video" :src="video" frameborder="0"></iframe>
 
-                    <div class="level-stats">
-                        <div>
-                            <div class="type-title-sm">Points</div>
+                    <ul class="stats">
+                        <li>
+                            <div class="type-title-sm">Points when completed</div>
                             <p>{{ score(selected + 1, 100, level.percentToQualify) }}</p>
-                        </div>
-
-                        <div>
+                        </li>
+                        <li>
                             <div class="type-title-sm">ID</div>
                             <p>{{ level.id }}</p>
-                        </div>
-
-                        <div>
+                        </li>
+                        <li>
                             <div class="type-title-sm">Rating</div>
                             <p>{{ level.rating }}</p>
-                        </div>
-                    </div>
+                        </li>
+                    </ul>
 
-                    <div class="ui-card">
-                        <h2>Victors ({{ level.victors.length }})</h2>
+                    <h2>Victors</h2>
 
-                        <div class="level-victors">
-                            <div
-                                v-for="victor in level.victors"
-                                class="ui-row"
-                            >
-                                {{ victor }}
-                            </div>
-                        </div>
-                    </div>
-
+                    <table class="victors">
+                        <tr v-for="victor in level.victors" :key="victor">
+                            <td>
+                                <span class="type-label-lg">{{ victor }}</span>
+                            </td>
+                        </tr>
+                    </table>
                 </div>
 
-                <div v-else class="ui-card" style="display:flex;align-items:center;justify-content:center;">
+                <div v-else class="level" style="height: 100%; display:flex; justify-content:center; align-items:center;">
                     <p>(ノಠ益ಠ)ノ彡┻━┻</p>
                 </div>
-
             </div>
-
         </main>
     `,
-
     data: () => ({
         list: [],
         editors: [],
         loading: true,
         selected: 0,
         errors: [],
+        roleIconMap,
         store
     }),
-
     computed: {
         level() {
             return this.list[this.selected]?.[0];
         },
         video() {
             if (!this.level) return "";
-            return embed(this.level.showcase || this.level.verification);
+            if (!this.level.showcase) {
+                return embed(this.level.verification);
+            }
+            return embed(
+                this.toggledShowcase
+                    ? this.level.showcase
+                    : this.level.verification
+            );
         },
     },
-
     async mounted() {
         this.list = await fetchList();
         this.editors = await fetchEditors();
+
+        if (!this.list) {
+            this.errors = [
+                "Failed to load list. Retry in a few minutes or notify list staff.",
+            ];
+        } else {
+            this.errors.push(
+                ...this.list
+                    .filter(([_, err]) => err)
+                    .map(([_, err]) => `Failed to load level. (${err}.json)`)
+            );
+            if (!this.editors) {
+                this.errors.push("Failed to load list editors.");
+            }
+        }
+
         this.loading = false;
     },
-
-    methods: { embed, score }
+    methods: {
+        embed,
+        score,
+    },
 };
