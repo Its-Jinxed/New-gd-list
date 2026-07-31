4b74912765e8d8fa878044db549cfc8c5e72ecea
Its-Jinxed <itsjinxed@outlook.com>
2026-07-31 12:14:59 +0100

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -1,9 +1,36 @@
 import { auth } from "./firebase.js";
 
 import {
-    GoogleAuthProvider,
-    signInWithPopup,
+    signInWithEmailAndPassword,
+    createUserWithEmailAndPassword,
     signOut,
     onAuthStateChanged
 } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-auth.js";
 
+export async function login(email, password) {
+    const userCredential = await signInWithEmailAndPassword(
+        auth,
+        email,
+        password
+    );
+
+    return userCredential.user;
+}
+
+export async function register(email, password) {
+    const userCredential = await createUserWithEmailAndPassword(
+        auth,
+        email,
+        password
+    );
+
+    return userCredential.user;
+}
+
+export async function logout() {
+    await signOut(auth);
+}
+
+export function watchUser(callback) {
+    onAuthStateChanged(auth, callback);
+}
