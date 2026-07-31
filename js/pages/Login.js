c973bbcbbf2467a3c7165cb0d5f48d3832b64182
Its-Jinxed <itsjinxed@outlook.com>
2026-07-31 12:15:18 +0100

    Update Login.js

--- js/pages/Login.js
+++ js/pages/Login.js
@@ -1,1 +1,54 @@
 import { login } from "../auth.js";
+
+export default {
+    data() {
+        return {
+            email: "",
+            password: "",
+            error: ""
+        };
+    },
+
+    methods: {
+        async signIn() {
+            this.error = "";
+
+            try {
+                await login(this.email, this.password);
+                alert("Logged in!");
+            } catch (err) {
+                this.error = err.message;
+            }
+        }
+    },
+
+    template: `
+        <main class="page">
+            <h1>Login</h1>
+
+            <input
+                v-model="email"
+                type="email"
+                placeholder="Email"
+            />
+
+            <br><br>
+
+            <input
+                v-model="password"
+                type="password"
+                placeholder="Password"
+            />
+
+            <br><br>
+
+            <button @click="signIn">
+                Login
+            </button>
+
+            <p v-if="error" style="color:red;">
+                {{ error }}
+            </p>
+        </main>
+    `
+};
