188fa273042855dfcf58c0741bedfdfa33fa48f0
Its-Jinxed <itsjinxed@outlook.com>
2026-07-31 12:11:42 +0100

    Create auth.js

---
+++ js/auth.js
@@ -0,0 +1,9 @@
+import { auth } from "./firebase.js";
+
+import {
+    GoogleAuthProvider,
+    signInWithPopup,
+    signOut,
+    onAuthStateChanged
+} from "https://www.gstatic.com/firebasejs/12.2.1/firebase-auth.js";
+
