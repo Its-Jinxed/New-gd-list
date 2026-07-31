90a66de1cc23db5f56d8ffd76b3698f8f698710f
Its-Jinxed <itsjinxed@outlook.com>
2026-07-31 12:24:15 +0100

    Update routes.js

--- js/routes.js
+++ js/routes.js
@@ -2,12 +2,10 @@ import List from './pages/List.js';
 import Leaderboard from './pages/Leaderboard.js';
 import Roulette from './pages/Roulette.js';
 import Packs from './pages/Packs.js';
-import Login from "./pages/Login.js";
 
 export default [
     { path: '/', component: List },
     { path: '/leaderboard', component: Leaderboard },
     { path: '/roulette', component: Roulette },
     { path: '/packs', component: Packs },
-    { path: "/login", component: Login },
 ];
